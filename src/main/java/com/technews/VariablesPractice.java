457158035b2b4cb335133a32ffa4101d3cf55155
Brett herrell <brett_herrell@yahoo.com>
2022-05-11 20:45:54 -0500

    message

---
+++ src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,13 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int number = 909;
+        double dub = 10.5;
+        double sum = number + dub;
+        String words = "yo ";
+        String wordnums = words + number;
+        System.out.println(wordnums);
+
+    }
+}
